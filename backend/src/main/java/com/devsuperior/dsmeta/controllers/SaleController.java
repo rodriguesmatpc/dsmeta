6f372997694bbee4a971b0f198f6c5f28077c986
Matheus Cavalcante <rodriguesmatpc@protonmail.com>
2022-07-17 11:35:54 -0300

    Twillio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sales;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,9 +19,18 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 
+	@Autowired
+	private SmsService smsService;
+
 	@GetMapping
 	public List<Sales> findaSales() {
 		return service.findSales();
+	}
+
+	@GetMapping("{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+
 	}
 
 }
